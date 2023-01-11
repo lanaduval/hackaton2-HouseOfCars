11e05ecb397e70a8e37b44e9bd4f4b2cc4cafc20
LAURE JANIN <ljanin51@gmail.com>
2023-01-11 19:30:47 +0100

    add navbar for filter for home

---
+++ frontend/src/components/FiltreBar/FiltreBar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+// import { Calendar } from "react-calendar";
+import "./FiltreBar.css";
+
+function FiltreBar() {
+  const [category, setCategory] = useState(null);
+  const [location, setLocation] = useState(null);
+  //   const [date, setDate] = useState(null);
+
+  return (
+    <div className="filtre_subnav">
+      <form className="form_filtre">
+        <label>
+          Category :
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">--</option>
+            <option value="confort">Confort</option>
+            <option value="casual">Casual</option>
+          </select>
+        </label>
+        <label>
+          Localisation :
+          <select
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="lyon">Lyon</option>
+            <option value="bordeaux">Bordeaux</option>
+            <option value="paris">Paris</option>
+            <option value="other">Autres</option>
+          </select>
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default FiltreBar;
--- frontend/src/components/layout-components/Navbar/Navbar.jsx
+++ frontend/src/components/layout-components/Navbar/Navbar.jsx
@@ -1,34 +1,30 @@
 import { Link } from "react-router-dom";
 import { stack as Menu } from "react-burger-menu";
-import logo from "../../../assets/logo.svg";
+import logo from "../../../assets/logo.png";
 import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <div id="outer-container">
       <header className="header">
+        <Menu outerContainerId="outer-container">
+          <Link to="/" className="menu-item">
+            Accueil
+          </Link>
+          <Link to="/about" className="menu-item">
+            About
+          </Link>
+          <Link to="/contact" className="menu-item">
+            Contact
+          </Link>
+        </Menu>
         <img className="logo" src={logo} alt="Logo" />
-        {/* <a className="bm-item" href="/">
-          Accueil
-        </a>
-        <a className="bm-item" href="/trucs">
-          Trucs de machins
-        </a>
-        <a className="bm-item" href="/contact">
-          Nous contacter
-        </a> */}
+        <Link to="/loggin">
+          <button type="button" className="login_button">
+            Login
+          </button>
+        </Link>
       </header>
-      <Menu outerContainerId="outer-container">
-        <Link to="/" className="menu-item">
-          Accueil
-        </Link>
-        <Link to="/about" className="menu-item">
-          About
-        </Link>
-        <Link to="/contact" className="menu-item">
-          Contact
-        </Link>
-      </Menu>
     </div>
   );
 }

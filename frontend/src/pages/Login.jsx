17e9ad0a66aa59cec50717d71d162484db06d850
lana duval <lanaduval09@gmail.com>
2023-01-12 02:04:33 +0100

    login page is linked to DB

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,23 +1,46 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "../assets/styles/Login.css";
+import instance from "../helpers/axios";
 
 function Login() {
-  
+  const [loginUser, setLoginUser] = useState("");
+  const navigate = useNavigate();
+
+  const handleChangeLogin = (e) => {
+    const { name, value } = e.target;
+    setLoginUser({ ...loginUser, [name]: value });
+  };
+  const handleLogin = (e) => {
+    e.preventDefault();
+    instance
+      .post("/login", loginUser)
+      .then(() => navigate("/CompanyPage"))
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className="Login">
-      <form className="loginForm">        
-      <input className="email"
+      <form className="loginForm" onSubmit={handleLogin}>
+        <input
+          className="email"
           type="email"
           name="email"
           placeholder="Email"
+          onChange={handleChangeLogin}
           required
         />
-        <input className="password"
+        <input
+          className="password"
           type="password"
           name="password"
           placeholder="Password"
+          onChange={handleChangeLogin}
           required
         />
-        <button className="SubmitLogin" type="submit">Login</button>
+        <button className="SubmitLogin" type="submit">
+          Login
+        </button>
       </form>
     </div>
   );

eaf91eb387a2dfbbc2dc74cbed5b6b4a986d388a
LAURE JANIN <ljanin51@gmail.com>
2023-01-12 20:05:02 +0100

    fix bug carroussel

--- frontend/src/components/caroussel/Caroussel.jsx
+++ frontend/src/components/caroussel/Caroussel.jsx
@@ -42,6 +42,8 @@ function Caroussel() {
     }
   };
 
+  
+
   // Nous avons la const filterCars qui permet de filtrer les voitures !
   const filterCars = cars.filter(
     (car) =>
@@ -53,6 +55,10 @@ function Caroussel() {
     currentPage * itemsPerPage,
     currentPage * itemsPerPage + itemsPerPage
   );
+
+  const showPrevious = currentPage !== 0;
+  const showNext =
+    currentPage < Math.ceil(filterCars.length / itemsPerPage) - 1;
 
   return (
     <>
@@ -92,7 +98,9 @@ function Caroussel() {
           type="button"
           className="button-previous"
           onClick={handlePrevious}
-          disabled={currentPage === 0}
+          disabled={!showPrevious}
+          // disabled={currentPage === 0}
+          style={{ display: showPrevious ? "block" : "none" }}
         >
           <img className="arrow" src={arrowLeft} alt="Arrow" />
         </button>
@@ -123,7 +131,9 @@ function Caroussel() {
           type="button"
           className="button-next"
           onClick={handleNext}
-          disabled={currentPage === Math.ceil(cars.length / itemsPerPage) - 1}
+          disabled={!showNext}
+          // disabled={currentPage === Math.ceil(cars.length / itemsPerPage) - 1}
+          style={{ display: showNext ? "block" : "none" }}
         >
           <img className="arrow" src={arrowRight} alt="Arrow" />
         </button>
